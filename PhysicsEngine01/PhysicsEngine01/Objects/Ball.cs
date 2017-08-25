70bd577240f43ead4ae56e8c6c03435dfb7078c7
Johan Lundgren <lundgren.johan@yahoo.se>
2017-08-25 11:46:35 +0200

    Added ball and physics folder

---
+++ PhysicsEngine01/PhysicsEngine01/Objects/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PhysicsEngine01.Objects
+{
+    class Ball
+    {
+        public Ball(Texture2D texture, Texture2D hitboxTexture, Vector2 position)
+        {
+
+        }
+    }
+}
--- PhysicsEngine01/PhysicsEngine01/Program.cs
+++ PhysicsEngine01/PhysicsEngine01/Program.cs
@@ -14,7 +14,7 @@ namespace PhysicsEngine01
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new GameEngine())
                 game.Run();
         }
     }
